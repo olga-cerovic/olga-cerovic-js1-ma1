548df59db90ce7ddda585fe3030d5c11827bdb48
olga-cerovic <olga.cerovic@stud.noroff.no>
2021-10-22 14:55:19 +0200

    Updates to script file
    Minor corrections to script.js

--- js/script.js
+++ js/script.js
@@ -35,13 +35,7 @@ heading.style.fontSize = "2em";
 
 heading.classList.add("subheading");
 
-/*
-Question 5:
-Write code that selects all the p elements on a page and assigns them to a 
-variable called paragraphs.
-
-Loop through the p elements and change the colour of each to "red".
-*/
+//Question 5:
 
 const paragraphs = document.querySelectorAll("p");
 
@@ -49,57 +43,24 @@ for (let i = 0; i < paragraphs.length; i++) {
   paragraphs[i].style.background = "red";
 }
 
-/*
-Question 6:
-Select the div with a class of results, assign it to a variable called 
-resultsContainer and set its inner HTML to be <p>New paragraph</p> and 
-its background colour to be yellow.
-*/
+//Question 6:
 
 const resultsContainer = document.querySelector(".results");
 
 resultsContainer.innerHTML = `<p>New paragraph</p>`;
 resultsContainer.style.background = "yellow";
 
-/*
-Question 7:
-Create a function that has one parameter called list.
-
-Inside the function, loop through the list parameter and console log the 
-name property in each object.
-
-Call the function and pass in the cats variable in the script.js file in the repo.
-*/
-
-///////////////////////////////////////////////////////////////////////////////////
+//Question 7:
 
 function displayListItems(list) {
   for (let i = 0; i < list.length; i++) {
-    console.log(list[i]);
+    console.log(list[i].name);
   }
 }
 
 displayListItems(cats);
 
-/*
-Question 8:
-Create a function called createCats. The function will have one parameter called cats.
-
-Inside the function loop through the value passed in as cats and create HTML for 
-each object in the array.
-
-Wrap each item in a div, each name property in an h5 tag and each age property 
-in a p tag.
-
-If the age property is missing, it should display “Age unknown” instead.
-
-Return the HTML from the function.
-
-Call the function and pass in the cats array as the argument.
-
-Assign the return value of the function to the innerHTML property of the element 
-on the HTML page with a class of cat-container.
-*/
+//Question 8:
 
 function createCats(cats) {
   console.log(cats);
@@ -119,6 +80,8 @@ function createCats(cats) {
   }
   return html;
 }
+
+createCats(cats);
 
 const updatedHTML = createCats(cats);
 
